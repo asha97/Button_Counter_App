3ca46d08cb00a626170c1ad02a23d3f75259cf33
Asha Islam <ashaislam31@gmail.com>
2023-01-18 19:12:05 -0500

    adding more "on" methods

--- app/src/main/java/com/example/buttoncounterapp/MainActivity.java
+++ app/src/main/java/com/example/buttoncounterapp/MainActivity.java
@@ -78,16 +78,52 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onStart() {
+        Log.d(TAG, "onStart: in");
         super.onStart();
+        Log.d(TAG, "onStart: out");
     }
 
     @Override
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        Log.d(TAG, "onRestoreInstanceState: in");
         super.onRestoreInstanceState(savedInstanceState);
+        Log.d(TAG, "onRestoreInstanceState: out");
     }
 
     @Override
     protected void onRestart() {
+        Log.d(TAG, "onRestart: in");
         super.onRestart();
+        Log.d(TAG, "onRestart: out");
     }
+
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause: in");
+        super.onPause();
+        Log.d(TAG, "onPause: out");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        Log.d(TAG, "onSaveInstanceState: in");
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState: out");
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume: in");
+        super.onResume();
+        Log.d(TAG, "onResume: out");
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop: in");
+        super.onStop();
+        Log.d(TAG, "onStop: out");
+    }
+
 }
